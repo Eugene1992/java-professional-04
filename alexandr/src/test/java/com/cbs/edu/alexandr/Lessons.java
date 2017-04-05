20558d8d45fb91891bc9f2329cbb6be2de251c43
Geruch1 <syrok@i.ua>
2017-04-05 17:51:21 +0300

    My first

---
+++ alexandr/src/test/java/com/cbs/edu/alexandr/Lessons.java
@@ -0,0 +1,10 @@
+package com.cbs.edu.alexandr;
+
+/**
+ * Created by Student on 05-Apr-17.
+ */
+public class Lessons {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
