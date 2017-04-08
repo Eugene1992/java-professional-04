7fa659ad73c15680f611b955a8da780e330c0fdf
Andryyyyy <loup@bigmir.net>
2017-04-08 10:11:21 +0300

    Test commit

---
+++ vladimir/src/main/java/com/cbs/edu/vladimir/cw01.java
@@ -0,0 +1,10 @@
+package com.cbs.edu.vladimir;
+
+/**
+ * Created by Student on 4/5/2017.
+ */
+public class cw01 {
+    public static void main(String[] args) {
+        System.out.println("Hello!!");
+    }
+}
