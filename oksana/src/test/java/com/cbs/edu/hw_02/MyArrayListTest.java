d468458feca183c8fac52b3e58c3ec403eacfe46
oksana <lnio@i.ua>
2017-04-10 12:22:46 +0300

    Second commit,MyArrayList implementation

--- oksana/src/test/java/com/cbs/edu/hw_02/MyArrayListTest.java
+++ oksana/src/test/java/com/cbs/edu/hw_02/MyArrayListTest.java
@@ -1,4 +1,127 @@
 package com.cbs.edu.hw_02;
 
-public class MyArrayList {
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class MyArrayListTest {
+    private MyArrayList<Integer> myArrayList;
+    private Integer value1;
+    private Integer value2;
+    private Integer value3;
+
+
+    @Before
+    public void setUp(){
+        myArrayList = new MyArrayList<>();
+        value1 = 45;
+        value2 = 50;
+        value3 = 3;
+    }
+
+    @Test
+    public void addTest1PlusGetTest(){
+
+        Assert.assertTrue(myArrayList.add(value2));
+        /*myArrayList.add(value1);
+        myArrayList.add(value2);
+       myArrayList.add(1,value3);
+        Integer element = myArrayList.get(1);
+        Assert.assertEquals(value3, element);*/
+
+    }
+//как написать тест для метода add(int index, T value), если он ничего не возвращает
+    @Test
+    public void addTest2(){
+
+
+    }
+
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void addWrongIndex() {
+        myArrayList.add(-5, value1);
+    }
+
+    //как написать тест, если не использовать метод add???
+    @Test
+    public void removeTest(){
+
+        myArrayList.add(value1);
+        myArrayList.add(value2);
+        myArrayList.add(1,value3);
+        myArrayList.remove(1);
+        Integer element = myArrayList.get(1);
+        Assert.assertEquals(value2, element);
+
+    }
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void removeWrongIndex() {
+        myArrayList.add(value1);
+        myArrayList.remove(-5);
+    }
+
+    @Test
+    public void containsTest(){
+        myArrayList.add(value1);
+        myArrayList.add(value2);
+        Assert.assertTrue(myArrayList.contains(value2));
+    }
+
+    @Test
+    public void setTest(){
+        myArrayList.add(value1);
+        myArrayList.add(value2);
+        Integer element = myArrayList.set(0, value3);
+        Assert.assertEquals(value3, element);
+
+    }
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void setWrongIndex() {
+        myArrayList.add(value1);
+        myArrayList.set(2, value3);
+    }
+
+    @Test
+    public void sizeTest(){
+        myArrayList.add(value1);
+        myArrayList.add(value2);
+        int s = myArrayList.size();
+        Assert.assertEquals(2, s);
+    }
+
+    @Test
+    public void iteratorTest(){
+        Assert.assertNotNull(myArrayList.iterator());
+    }
+
+    @Test
+    public void hasNextTest(){
+        myArrayList.add(value1);
+        Assert.assertTrue(myArrayList.iterator().hasNext());
+    }
+
+    @Test
+    public void nextTest(){
+        myArrayList.add(value1);
+        Assert.assertNotNull(myArrayList.iterator().next());
+        Integer element = myArrayList.iterator().next();
+        Assert.assertEquals(value1, element);
+    }
+
+    @Test
+    public void countSymbolTest(){
+        Integer count = 11;
+        Assert.assertNotNull(myArrayList.countSymbol());
+        Assert.assertEquals(count, myArrayList.countSymbol().get("Hello, Java"));
+    }
+
+
+
+
+
+
+
+
 }
