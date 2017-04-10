7f1ec9a3a9a3450742450b0307db56145c7f3be9
Eugene1992 <deyneko55@gmail.com>
2017-04-10 18:43:45 +0300

    Added some examples.

--- src/main/java/com/cbs/edu/examples/cw_03/Test.java
+++ src/main/java/com/cbs/edu/examples/cw_03/Test.java
@@ -1,4 +1,28 @@
 package com.cbs.edu.examples.cw_03;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 public class Test {
+    public static void main(String[] args) {
+        HashMap<Integer, String> objects = new HashMap<>();
+
+        objects.put(1, "One");
+        objects.put(2, "Two");
+        objects.put(3, "Three");
+        objects.put(1000, "Thousand");
+        objects.put(100, "Hundred");
+
+        Set<Map.Entry<Integer, String>> entries = objects.entrySet();
+        for (Map.Entry<Integer, String> entry : entries) {
+
+        }
+
+        Set<Integer> integers = objects.keySet();
+        Collection<String> values = objects.values();
+
+        System.out.println(objects);
+    }
 }
