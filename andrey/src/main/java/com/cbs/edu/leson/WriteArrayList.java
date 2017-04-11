3f6435167968bc02e3fbb4d9704e8130b1a066ee
Eugene1992 <deyneko55@gmail.com>
2017-04-11 23:05:55 +0300

    Added checkstyle plugin. Refactored all modules for checkstyle valid checks.

--- andrey/src/main/java/com/cbs/edu/leson/WriteArrayList.java
+++ andrey/src/main/java/com/cbs/edu/leson/WriteArrayList.java
@@ -1,109 +1,145 @@
 package com.cbs.edu.leson;
 
-/**
- • add(T value) - Adds an element to the end of an array
- • add(int index, T value) - inserts the element into the specified position all items on the left are moved to one element
- • get(int index) gives an instance of an element at the specified index
- • set(int index, T value) - changes the element in the specified position to the one that is transferred
- • contains(T value)  -  determines the presence of at least some values
- • remove(int index) - removes the specified object and all elements that are "to the right" are moved one cell to the left
- • iterator()
- • size()
- */
-
 import java.util.EmptyStackException;
 import java.util.Iterator;
 
+/**
+ * Custom ArrayList implementation.
+ * @param <T> genetic type
+ */
 public class WriteArrayList<T> implements Iterable<T> {
-    private static final int startSize = 1;
+    private static final int START_SIZE = 1;
     private T[] arr;
-    public int top;
+    private int top;
 
-    public WriteArrayList(){
+    public WriteArrayList() {
         this.top = 1;
-        this.arr = (T[]) new Object[startSize];
+        this.arr = (T[]) new Object[START_SIZE];
     }
 
-    // add - Adds an element to the end of an array
-
-    public T add(T object){
+    /**
+     * Adds an element to the end of an array.
+     *
+     * @param object added object
+     * @return saved object
+     */
+    public T add(T object) {
         if (top == arr.length) {
-            T[] newArr = (T[]) new Object[(this.arr.length*3)/2 + 1];
+            final int newSize = (this.arr.length * 3) / 2 + 1;
+            T[] newArr = (T[]) new Object[newSize];
             System.arraycopy(this.arr, 0, newArr, 0, this.top);
             this.arr = newArr;
         }
         this.arr[top++] = object;
         return object;
     }
-    // add(int index,T object)- inserts the element into the specified position all items on the left are moved to one element
 
-    public T add(int index,T object) throws ArrayIndexOutOfBoundsException{
-        if ((index > 0 ) | (index < this.top) ){
+    /**
+     * Inserts the element into the specified position all items on the left are moved to one element.
+     *
+     * @param index specified insert index
+     * @param object added object
+     * @return saved object
+     * @throws ArrayIndexOutOfBoundsException if specified index is illegal
+     */
+    public T add(int index, T object) throws ArrayIndexOutOfBoundsException {
+        if ((index > 0) | (index < this.top)) {
             if (top == (arr.length)) {
-                T[] newArr = (T[]) new Object[(this.arr.length*3)/2 + 1];
+                final int newSize = (this.arr.length * 3) / 2 + 1;
+                T[] newArr = (T[]) new Object[newSize];
                 System.arraycopy(this.arr, 0, newArr, 0, this.top);
                 this.arr = newArr;
             }
-        for (int numberShear = top; numberShear < index; numberShear--){
-            this.arr[numberShear+1]=this.arr[numberShear];}
-         this.arr[index]= object;
+            for (int numberShear = top; numberShear < index; numberShear--) {
+                this.arr[numberShear + 1] = this.arr[numberShear];
+            }
+            this.arr[index] = object;
             top++;
+        } else {
+            throw new ArrayIndexOutOfBoundsException();
         }
-        else  throw new ArrayIndexOutOfBoundsException();
         return object;
-
     }
 
-    // get - gives an instance of an element at the specified index
-
-    public T get(int index) throws ArrayIndexOutOfBoundsException{
-        if (index > 0 | index <= top ){
+    /**
+     * Gives an instance of an element at the specified index.
+     *
+     * @param index specified index for retrieved object
+     * @return founded object
+     * @throws ArrayIndexOutOfBoundsException if specified index is illegal
+     */
+    public T get(int index) throws ArrayIndexOutOfBoundsException {
+        if (index > 0 | index <= top) {
             T elem = this.arr[index];
-           /* for (int numberShear = index; numberShear < top; numberShear++){
-                this.arr[numberShear]=this.arr[numberShear+1];} */
             return elem;
+        } else {
+            throw new EmptyStackException();
         }
-        else  throw new EmptyStackException();
     }
 
-    // set - changes the element in the specified position to the one that is transferred
-
-    public T set(int index,T object)throws ArrayIndexOutOfBoundsException{
-        if (index > 0 | index<=top ){
-        this.arr[index]= object;
-        return object;
+    /**
+     * Changes the element in the specified position to the one that is transferred.
+     *
+     * @param index specified updated object index
+     * @param object updated object
+     * @return updated object
+     * @throws ArrayIndexOutOfBoundsException if specified index is illegal
+     */
+    public T set(int index, T object) throws ArrayIndexOutOfBoundsException {
+        if (index > 0 | index <= top) {
+            this.arr[index] = object;
+            return object;
+        } else {
+            throw new ArrayIndexOutOfBoundsException();
         }
-        else  throw new ArrayIndexOutOfBoundsException();
-
     }
 
-    //contains - determines the presence of at least some values
-
-    public boolean contains(T object){
-         boolean bul= false;
-        for (int number = 1; number < top; number++){
-        if (this.arr[number] != null){
-             bul=true;
-        } else bul=false;
-        }    return bul;
+    /**
+     * Determines the presence of at least some values.
+     *
+     * @param object specified object
+     * @return comparing result
+     */
+    public boolean contains(T object) {
+        boolean bul = false;
+        for (int number = 1; number < top; number++) {
+            if (this.arr[number] != null) {
+                bul = true;
+            } else {
+                bul = false;
+            }
+        }
+        return bul;
     }
 
-    //  remove - removes the specified object and all elements that are "to the right" are moved one cell to the left
-
-    public void remove(int index)throws ArrayIndexOutOfBoundsException{
-        if (index >= 0 | index<top ){
-        for (int numberShear = index; numberShear < top; numberShear++){
-            this.arr[numberShear]=this.arr[numberShear+1];}
-        this.arr[top]= null;
+    /**
+     * Removes the specified object and all elements that are "to the right" are moved one cell to the left.
+     *
+     * @param index specified remove object index
+     * @throws ArrayIndexOutOfBoundsException if specified index is illegal
+     */
+    public void remove(int index) throws ArrayIndexOutOfBoundsException {
+        if (index >= 0 | index < top) {
+            for (int numberShear = index; numberShear < top; numberShear++) {
+                this.arr[numberShear] = this.arr[numberShear + 1];
+            }
+            this.arr[top] = null;
             top--;
-    }     else  throw new ArrayIndexOutOfBoundsException();
+        } else {
+            throw new ArrayIndexOutOfBoundsException();
+        }
     }
+
     @Override
-    public Iterator<T>  iterator(){
+    public Iterator<T> iterator() {
         return new MyIterator();
     }
+
+    /**
+     * Iterator implementation.
+     */
     private class MyIterator implements Iterator<T> {
-        int current = -1;
+        private int current = -1;
 
         public boolean hasNext() {
             return current + 1 < top;
@@ -117,15 +153,8 @@ public class WriteArrayList<T> implements Iterable<T> {
             arr[current] = null;
         }
     }
-    public int  size(){
+
+    public int size() {
         return top;
     }
-
-  //  public Map<int,T>   givemap(T objact){
-   //     Map<String, T> staff = new HashMap<String, T>();
-   //     for (int number =0 ; number < top; number++){
-    //        map.put(number,this.arr[number]}
-  //  }
-
-
 }
