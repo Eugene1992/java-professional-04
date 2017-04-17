386a06eff21cad1be3c7620082d0c5c5984a932f
oksana <lnio@i.ua>
2017-04-17 23:01:01 +0300

    Implementation MyLinkedList with tests

--- oksana/src/main/java/com/cbs/edu/hw_03/MyLinkedList.java
+++ oksana/src/main/java/com/cbs/edu/hw_03/MyLinkedList.java
@@ -76,19 +76,20 @@ public class MyLinkedList<T> {
         return true;
     }
 
-     /**
-      * Inserts the specified element at the specified position in this list.
-      * @param index   specified insert index
-      * @param element added object
-      * @throws IndexOutOfBoundsException if specified index is illegal
-      */
+    /**
+     * Inserts the specified element at the specified position in this list.
+     *
+     * @param index   specified insert index
+     * @param element added object
+     * @throws IndexOutOfBoundsException if specified index is illegal
+     */
     public void addByIndex(int index, T element) throws IndexOutOfBoundsException {
         checkIndexOutOfBoundsException(index);
         Node<T> nextObject = head;
-        for(int i = index; i > 0; i--){
+        for (int i = index; i > 0; i--) {
             nextObject = nextObject.next;
         }
-        if(index == 0) {
+        if (index == 0) {
             addFirst(element);
         }
         Node<T> prevObject = nextObject.prev;
@@ -106,7 +107,7 @@ public class MyLinkedList<T> {
      */
     public boolean contains(T value) {
         Node<T> nextObject = this.head;
-        for (int i = 0; i <= size-1; i++) {
+        for (int i = 0; i <= size - 1; i++) {
             if (nextObject.element.equals(value)) {
                 return true;
             }
@@ -115,7 +116,8 @@ public class MyLinkedList<T> {
         return false;
     }
 
-    /**Changes the element in the specified position to the one that is transferred.
+    /**
+     * Changes the element in the specified position to the one that is transferred.
      *
      * @param index specified updated object index
      * @param value updated object
@@ -124,7 +126,7 @@ public class MyLinkedList<T> {
     public T set(int index, T value) throws IndexOutOfBoundsException {
         checkIndexOutOfBoundsException(index);
         Node<T> nextObject = head;
-        if(index == 0) {
+        if (index == 0) {
             nextObject.element = value;
             return nextObject.element;
         } else {
@@ -136,7 +138,8 @@ public class MyLinkedList<T> {
         }
     }
 
-    /**remove(int index) Removes the element at the specified position in this list.
+    /**
+     * remove(int index) Removes the element at the specified position in this list.
      *
      * @param index specified remove object index
      * @throws IndexOutOfBoundsException if specified index is illegal
@@ -145,10 +148,10 @@ public class MyLinkedList<T> {
         checkIndexOutOfBoundsException(index);
         Node<T> nextObject = head;
         T element = null;
-        if(index == 0) {
+        if (index == 0) {
             element = nextObject.element;
-             head = nextObject.next;
-             size--;
+            head = nextObject.next;
+            size--;
             return element;
         } else {
             for (int i = index; i > 0; i--) {
@@ -172,7 +175,7 @@ public class MyLinkedList<T> {
     public T get(int index) throws IndexOutOfBoundsException {
         checkIndexOutOfBoundsException(index);
         Node<T> nextObject = head;
-        if(index == 0) {
+        if (index == 0) {
             return nextObject.element;
         } else {
             for (int i = index; i > 0; i--) {
@@ -191,7 +194,8 @@ public class MyLinkedList<T> {
         return size;
     }
 
-    /**iterator() Returns an iterator over the elements in this list in proper sequence.
+    /**
+     * iterator() Returns an iterator over the elements in this list in proper sequence.
      *
      * @return object Iterator
      */
@@ -206,25 +210,33 @@ public class MyLinkedList<T> {
         private int current = -1;
         private Node<T> currentObject = null;
 
+        /**
+         * Returns true if the iteration has more elements.
+         *
+         */
         public boolean hasNext() {
             return current + 1 < size;
         }
-
-        public T next() throws NoSuchElementException{
+        /**
+         * Returns the next element in the iteration.
+         *
+         * @return object T
+         */
+        public T next() throws NoSuchElementException {
             if (!this.hasNext()) {
                 throw new NoSuchElementException();
             } else {
-                if(currentObject == null) {
+                if (currentObject == null) {
                     currentObject = head;
                 } else {
                     currentObject = currentObject.next;
                 }
-                    ++this.current;
-                    return currentObject.element;
-                }
+                ++this.current;
+                return currentObject.element;
             }
         }
     }
+}
 
 
 
