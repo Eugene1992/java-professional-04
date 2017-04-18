09ac353ead1577f99ec9b63e33ae508ed4306383
Andryyyyy <loup@bigmir.net>
2017-04-18 23:00:52 +0300

    image reader gud

--- andrey/src/main/java/com/cbs/edu/leson/Entry.java
+++ andrey/src/main/java/com/cbs/edu/leson/Entry.java
@@ -2,11 +2,11 @@ package com.cbs.edu.leson;
 /*@param <T> - Indicates the type of the variable
   Entry - element linkedList
  */
-public class Entry<T> {
+public class Entry<T>{
         private T element;
         private Entry<T> next;
         private Entry<T> prev;
-        Entry(T element, Entry<T> next, Entry<T> prev){
+        Entry(T element,Entry<T>next,Entry<T>prev){
             this.element = element;
             this.next = next;
             this.prev = prev;
