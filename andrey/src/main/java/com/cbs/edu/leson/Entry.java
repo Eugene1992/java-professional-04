12ff29009be2288471eb6d4daf64f4383ff84991
Andryyyyy <loup@bigmir.net>
2017-04-18 22:46:31 +0300

    image reader gud

--- andrey/src/main/java/com/cbs/edu/leson/Entry.java
+++ andrey/src/main/java/com/cbs/edu/leson/Entry.java
@@ -2,10 +2,9 @@ package com.cbs.edu.leson;
 //@param <T> - Indicates the type of the variable
 public class Entry<T>{
         private T element;
-        Entry<T> next;
-        Entry<T> prev;
-        Entry(T element, Entry<T> next, Entry<T> prev)
-        {
+        private Entry<T> next;
+        private Entry<T> prev;
+        Entry(T element, Entry<T> next, Entry<T> prev){
             this.element = element;
             this.next = next;
             this.prev = prev;
