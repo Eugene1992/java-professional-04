e148b500c84a41a72174d60b3c4ea5ab64572567
Eugene1992 <deyneko55@gmail.com>
2017-04-19 18:52:21 +0300

    Added dima classwork.

---
+++ dima/src/main/java/homework/cw07/Employee.java
@@ -0,0 +1,17 @@
+package homework.cw07;
+
+public class Employee {
+    @NotNull
+    @Size(value = 20, message = "max size")
+    private String name;
+    @NotNull
+    @Min(value = 18, message = "Min Age")
+    @Max(value = 40, message = "Max Age")
+    private int age;
+    @NotNull
+    @Min(value = 100, message = "Min Height")
+    @Max(value = 250, message = "Max Height")
+    private float height;
+    @NotNull
+    private boolean isMarried;
+}
