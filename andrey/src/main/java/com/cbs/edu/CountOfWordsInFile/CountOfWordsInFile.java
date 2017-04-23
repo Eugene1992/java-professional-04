e7496f75091349a82eb39bb427502d3b0a07aefb
Andryyyyy <loup@bigmir.net>
2017-04-23 21:48:33 +0300

    image reader gud

--- andrey/src/main/java/com/cbs/edu/CountOfWordsInFile/CountOfWordsInFile.java
+++ andrey/src/main/java/com/cbs/edu/CountOfWordsInFile/CountOfWordsInFile.java
@@ -2,7 +2,10 @@ package com.cbs.edu.CountOfWordsInFile;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -18,28 +21,29 @@ public class CountOfWordsInFile {
      * FileReaderWithText - class returns a text variable into which it reads everything written into the file at the transmitted address
      *
      * @param pathToFile   - path to the file with text
-     * @param textFromFile - contains all the text in the file
+     * @param wordsExclude - contains all the text in the file
      */
-    public static String fileReaderWithText(String pathToFile,String wordsExclude) {
-        HashSet<String> exceptionWordSet = new HashSet<>();
+    public static String fileReaderWithText(String pathToFile, String wordsExclude) {
+        Set exceptionSet = new HashSet<String>();
         String textFromFile = null;
-        try(FileReader reader = new FileReader(wordsExclude))
-        {
-            int position;
-            String word = null;
-            while((position=reader.read())!=-1){
-                if (letterVerification((char)position)){
-                    System.out.println((char)position);
-                    exceptionWordSet.add(word);
+        Map mapWord = new HashMap<String, Integer>();
+        try (FileReader reader = new FileReader(wordsExclude)) {
+            int position = 0;
+            String word = "";
+            while ((position = reader.read()) != -1) {
+                word = word + ((char) position);
+                if (letterVerification((char) position)) {
+                    exceptionSet.add(word);
+                    System.out.println(word);
+                    word = "";
                 } else {
-                    word = word + ((char) position);
+
+
                 }
 
             }
-        }
-        catch(IOException ex){
+        } catch (IOException ex) {
 
-            System.out.println(ex.getMessage());
         }
 
         try (FileReader reader = new FileReader(pathToFile)) {
@@ -48,21 +52,27 @@ public class CountOfWordsInFile {
             while ((position = reader.read()) != -1) {
                 char letter = (char) position;
                 if (letterVerification(letter)) {
-                    if (checkWithRegExp(word)& !(exceptionWordSet.contains(word))){
-                    textFromFile = textFromFile + word + " ";
-                    System.out.print(word + " ");
-                   }
-                    word ="";
+                    if (checkWithRegExp(word) & word.compareTo("html") != 0 & word.compareTo("link") != 0 & word.compareTo("meta") != 0) {
+                        int value = 1;
+                        if (mapWord.containsKey(word)){
+                            textFromFile = textFromFile + word + " ";
+                             value = (int)mapWord.get(word);
+                            mapWord.put(word,value+1);
+                            System.out.print(word + " ");
+                        } else
+                            mapWord.put(word,1);
+                    }
+                    word = "";
                 } else {
-              word = word + ((char) position);
+                    word = word + ((char) position);
                 }
             }
 
 
         } catch (IOException ex) {
-            return ex.getMessage();
+
         }
-        return  textFromFile;
+        return textFromFile;
     }
 
     public static boolean checkWithRegExp(String userNameString) {
--- andrey/src/test/java/com/cbs/edu/CountOfWordsInFile/CountOfWordsInFileTest.java
+++ andrey/src/test/java/com/cbs/edu/CountOfWordsInFile/CountOfWordsInFileTest.java
@@ -11,8 +11,8 @@ import org.junit.Test;
  */
 public class CountOfWordsInFileTest {
     @Test
-   public void ileReaderWithTextTest (){
-       String a =CountOfWordsInFile.fileReaderWithText("C:\\Users\\Волк\\Desktop\\java-professional-04\\andrey\\src\\main\\java\\com\\cbs\\edu\\CountOfWordsInFile\\Game.of.Thrones.-.03x02.txt","C:\\Users\\Волк\\Desktop\\java-professional-04\\andrey\\src\\main\\java\\com\\cbs\\edu\\CountOfWordsInFile\\WordsExclude.txt");
-      System.out.println(a);
+    public void ileReaderWithTextTest() {
+        String a = CountOfWordsInFile.fileReaderWithText("C:\\Users\\Волк\\Desktop\\java-professional-04\\andrey\\src\\main\\java\\com\\cbs\\edu\\CountOfWordsInFile\\Game.of.Thrones.-.03x02.txt", "C:\\Users\\Волк\\Desktop\\java-professional-04\\andrey\\src\\main\\java\\com\\cbs\\edu\\CountOfWordsInFile\\WordsExclude.txt");
+        System.out.println(a);
     }
 }
